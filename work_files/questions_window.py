a94863c5204bdd35ef04b0e78d04b60ee93444bb
DelZeiT <maksimcvetkov1106@gmail.com>
2023-08-10 16:22:00 +0300

    Добавил открытие окна со вторым вопросом и прибавление 100000 к балансу после выбора правильного (первого) ответа и убавление жизней в случае неправильного ответа.

--- work_files/questions_window.py
+++ work_files/questions_window.py
@@ -1,18 +1,16 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import *
+
+balance = 0    # баланс
+count_life = 3    # счетчик жизней
 
 
 # функция открытия окна с вопросами
 def open_question_window1():
     Form2, Window2 = uic.loadUiType("C:/_PYTHON_PROJECT_/How_to_become_millioner_py/ui_ui_files/questions_window_ui.ui")
-
+    global form2
     window2 = Window2()
     form2 = Form2()
     form2.setupUi(window2)
-    window2.exec()
-
-    balance = 0
-    count_life = 3
 
     # кнопки
     btn1 = form2.pushButton
@@ -27,28 +25,45 @@ def open_question_window1():
     btn4 = form2.pushButton_4
     btn4.clicked.connect(clicked_btn4)
 
+    window2.exec_()
 
-def clicked_btn1(balance, form2):
+
+# функция нажатия кнопки с первым вариантом ответа
+def clicked_btn1():
+    global balance
     balance += 100000
-    form2.lineEdit_balance.insert(balance)
+    form2.lineEdit_2.setText(str(balance))
     open_question_window2()
 
 
+# функция нажатия кнопки со вторым вариантом ответа
 def clicked_btn2():
+    global count_life
+    count_life -= 1
+    form2.lineEdit.setText(str(count_life))
     open_question_window2()
 
 
+# функция нажатия кнопки с третьим вариантом ответа
 def clicked_btn3():
+    global count_life
+    count_life -= 1
+    form2.lineEdit.setText(str(count_life))
     open_question_window2()
 
 
+# функция нажатия кнопки с четвертым вариантом ответа
 def clicked_btn4():
+    global count_life
+    count_life -= 1
+    form2.lineEdit.setText(str(count_life))
     open_question_window2()
 
 
+# функция открытия окна со вторым вопросом
 def open_question_window2():
     Form3, Window3 = uic.loadUiType("C:/_PYTHON_PROJECT_/How_to_become_millioner_py/ui_ui_files/question_window2_ui.ui")
-
+    global form3
     window3 = Window3()
     form3 = Form3()
     form3.setupUi(window3)
